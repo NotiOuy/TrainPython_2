ca6885a2558e837ff14dd3d62c379c48a169e809
NotiOuy <immaxnor@gmail.com>
2022-03-17 21:21:39 +0200

    add feature index_operator.py

---
+++ index_operator.py
@@ -0,0 +1,9 @@
+
+# index operator [] = gives access to a sequence's element (str, list, tuples)
+
+greet = "hey, man!"
+
+first_word = greet[0:3].upper()
+last_word = greet[5:8].lower()
+
+print(first_word, last_word)
